4b49cb2f04f11f7c599b35d214f746c411cb5a34
jim-works <jim4m80@gmail.com>
2023-06-12 09:35:36 -0500

    i've copied enough code so pbr works now. also, block textures now repeat when scaled (hopefully)

--- assets/shaders/array_texture.wgsl
+++ assets/shaders/array_texture.wgsl
@@ -1,16 +1,3 @@
-// #import bevy_pbr::mesh_view_bindings
-// #import bevy_pbr::mesh_bindings
-
-// //#import bevy_pbr::pbr_types
-// #import bevy_pbr::pbr_bindings
-// #import bevy_pbr::utils
-// #import bevy_pbr::clustered_forward
-// #import bevy_pbr::lighting
-// #import bevy_pbr::shadows
-// #import bevy_pbr::fog
-// #import bevy_pbr::pbr_functions
-// #import bevy_pbr::pbr_ambient
-
 #import bevy_pbr::mesh_view_bindings
 #import bevy_pbr::pbr_bindings
 #import bevy_pbr::mesh_bindings
@@ -26,9 +13,9 @@
 #import bevy_pbr::prepass_utils
 #import bevy_pbr::mesh_functions
 
-@group(1) @binding(0)
-var my_array_texture: texture_2d_array<f32>;
 @group(1) @binding(1)
+var my_array_texture: texture_2d_array<f32>;
+@group(1) @binding(2)
 var my_array_texture_sampler: sampler;
 
 //from https://github.com/bevyengine/bevy/blob/527d3a5885daa4b43df7054f7787dad47f06135d/crates/bevy_pbr/src/render/mesh.wgsl
@@ -115,38 +102,136 @@ struct FragmentInput {
 
 @fragment
 fn fragment(in: FragmentInput) -> @location(0) vec4<f32> {
-    // Prepare a 'processed' StandardMaterial by sampling all textures to resolve
-    // the material members
     //TODO: update according to https://github.com/bevyengine/bevy/blob/527d3a5885daa4b43df7054f7787dad47f06135d/crates/bevy_pbr/src/render/pbr.wgsl
-    var pbr_input: PbrInput = pbr_input_new();
-
-    //https://www.w3.org/TR/WGSL/#texturesample
-    pbr_input.material.base_color = textureSample(my_array_texture, my_array_texture_sampler, in.uv, in.layer);
+    let is_orthographic = view.projection[3].w == 1.0;
+    let V = calculate_view(in.world_position, is_orthographic);
+#ifdef VERTEX_UVS
+    var uv = in.uv;
+#ifdef VERTEX_TANGENTS
+    if ((material.flags & STANDARD_MATERIAL_FLAGS_DEPTH_MAP_BIT) != 0u) {
+        let N = in.world_normal;
+        let T = in.world_tangent.xyz;
+        let B = in.world_tangent.w * cross(N, T);
+        // Transform V from fragment to camera in world space to tangent space.
+        let Vt = vec3(dot(V, T), dot(V, B), dot(V, N));
+        uv = parallaxed_uv(
+            material.parallax_depth_scale,
+            material.max_parallax_layer_count,
+            material.max_relief_mapping_search_steps,
+            uv,
+            // Flip the direction of Vt to go toward the surface to make the
+            // parallax mapping algorithm easier to understand and reason
+            // about.
+            -Vt,
+        );
+    }
+#endif
+#endif
+    var output_color: vec4<f32> = material.base_color;
 #ifdef VERTEX_COLORS
-    pbr_input.material.base_color = pbr_input.material.base_color * in.color;
+    output_color = output_color * in.color;
+#endif
+#ifdef VERTEX_UVS
+    if ((material.flags & STANDARD_MATERIAL_FLAGS_BASE_COLOR_TEXTURE_BIT) != 0u) {
+        output_color = output_color * textureSample(my_array_texture, my_array_texture_sampler, uv, in.layer);
+    }
 #endif
 
-    pbr_input.frag_coord = in.frag_coord;
-    pbr_input.world_position = in.world_position;
-    pbr_input.world_normal = prepare_world_normal(
-        in.world_normal,
-        (pbr_input.material.flags & STANDARD_MATERIAL_FLAGS_DOUBLE_SIDED_BIT) != 0u,
-        in.is_front,
-    );
+    // NOTE: Unlit bit not set means == 0 is true, so the true case is if lit
+    if ((material.flags & STANDARD_MATERIAL_FLAGS_UNLIT_BIT) == 0u) {
+        // Prepare a 'processed' StandardMaterial by sampling all textures to resolve
+        // the material members
+        var pbr_input: PbrInput;
 
-    pbr_input.is_orthographic = view.projection[3].w == 1.0;
+        pbr_input.material.base_color = output_color;
+        pbr_input.material.reflectance = material.reflectance;
+        pbr_input.material.flags = material.flags;
+        pbr_input.material.alpha_cutoff = material.alpha_cutoff;
 
-    pbr_input.N = apply_normal_mapping(
-        pbr_input.material.flags,
-        pbr_input.world_normal,
+        // TODO use .a for exposure compensation in HDR
+        var emissive: vec4<f32> = material.emissive;
+#ifdef VERTEX_UVS
+        if ((material.flags & STANDARD_MATERIAL_FLAGS_EMISSIVE_TEXTURE_BIT) != 0u) {
+            emissive = vec4<f32>(emissive.rgb * textureSample(emissive_texture, emissive_sampler, uv).rgb, 1.0);
+        }
+#endif
+        pbr_input.material.emissive = emissive;
+
+        var metallic: f32 = material.metallic;
+        var perceptual_roughness: f32 = material.perceptual_roughness;
+#ifdef VERTEX_UVS
+        if ((material.flags & STANDARD_MATERIAL_FLAGS_METALLIC_ROUGHNESS_TEXTURE_BIT) != 0u) {
+            let metallic_roughness = textureSample(metallic_roughness_texture, metallic_roughness_sampler, uv);
+            // Sampling from GLTF standard channels for now
+            metallic = metallic * metallic_roughness.b;
+            perceptual_roughness = perceptual_roughness * metallic_roughness.g;
+        }
+#endif
+        pbr_input.material.metallic = metallic;
+        pbr_input.material.perceptual_roughness = perceptual_roughness;
+
+        var occlusion: f32 = 1.0;
+#ifdef VERTEX_UVS
+        if ((material.flags & STANDARD_MATERIAL_FLAGS_OCCLUSION_TEXTURE_BIT) != 0u) {
+            occlusion = textureSample(occlusion_texture, occlusion_sampler, uv).r;
+        }
+#endif
+        pbr_input.frag_coord = in.frag_coord;
+        pbr_input.world_position = in.world_position;
+
+#ifdef LOAD_PREPASS_NORMALS
+        pbr_input.world_normal = prepass_normal(in.frag_coord, 0u);
+#else // LOAD_PREPASS_NORMALS
+        pbr_input.world_normal = prepare_world_normal(
+            in.world_normal,
+            (material.flags & STANDARD_MATERIAL_FLAGS_DOUBLE_SIDED_BIT) != 0u,
+            in.is_front,
+        );
+#endif // LOAD_PREPASS_NORMALS
+
+        pbr_input.is_orthographic = is_orthographic;
+
+        pbr_input.N = apply_normal_mapping(
+            material.flags,
+            pbr_input.world_normal,
 #ifdef VERTEX_TANGENTS
 #ifdef STANDARDMATERIAL_NORMAL_MAP
-        in.world_tangent,
+            in.world_tangent,
 #endif
 #endif
-        in.uv,
-    );
-    pbr_input.V = calculate_view(in.world_position, pbr_input.is_orthographic);
+#ifdef VERTEX_UVS
+            uv,
+#endif
+        );
+        pbr_input.V = V;
+        pbr_input.occlusion = occlusion;
 
-    return tone_mapping(pbr(pbr_input));
+        pbr_input.flags = mesh.flags;
+
+        output_color = pbr(pbr_input);
+    } else {
+        output_color = alpha_discard(material, output_color);
+    }
+
+    // fog
+    if (fog.mode != FOG_MODE_OFF && (material.flags & STANDARD_MATERIAL_FLAGS_FOG_ENABLED_BIT) != 0u) {
+        //temp disable output_color = apply_fog(fog, output_color, in.world_position.xyz, view.world_position.xyz);
+    }
+
+#ifdef TONEMAP_IN_SHADER
+    output_color = tone_mapping(output_color);
+#ifdef DEBAND_DITHER
+    var output_rgb = output_color.rgb;
+    output_rgb = powsafe(output_rgb, 1.0 / 2.2);
+    output_rgb = output_rgb + screen_space_dither(in.frag_coord.xy);
+    // This conversion back to linear space is required because our output texture format is
+    // SRGB; the GPU will assume our output is linear and will apply an SRGB conversion.
+    output_rgb = powsafe(output_rgb, 2.2);
+    output_color = vec4(output_rgb, output_color.a);
+#endif
+#endif
+#ifdef PREMULTIPLY_ALPHA
+    output_color = premultiply_alpha(material.flags, output_color);
+#endif
+    return output_color;
 }
--- src/mesher/mod.rs
+++ src/mesher/mod.rs
@@ -3,24 +3,43 @@ pub use mesher::*;
 
 mod mesh_lod;
 
-use bevy::{prelude::*, render::{render_resource::{AsBindGroup, ShaderRef, VertexFormat, RenderPipelineDescriptor, SpecializedMeshPipelineError}, texture::ImageSampler, mesh::{MeshVertexAttribute, MeshVertexBufferLayout}}, reflect::TypeUuid, asset::LoadState, pbr::*};
+use bevy::{
+    asset::LoadState,
+    pbr::*,
+    prelude::*,
+    reflect::TypeUuid,
+    render::{
+        mesh::{MeshVertexAttribute, MeshVertexBufferLayout},
+        render_resource::{
+            AddressMode, AsBindGroup, RenderPipelineDescriptor, ShaderRef,
+            SpecializedMeshPipelineError, VertexFormat, Face, TextureFormat, AsBindGroupShaderType, ShaderType,
+        },
+        texture::ImageSampler, render_asset::RenderAssets,
+    },
+};
 
-use crate::world::{LevelSystemSet, chunk};
+use crate::world::{chunk, LevelSystemSet};
 
 pub struct MesherPlugin;
 
 const SPAWN_MESH_TIME_BUDGET_COUNT: u32 = 1000;
 
-
 impl Plugin for MesherPlugin {
     fn build(&self, app: &mut App) {
-        app
-            .add_plugin(MaterialPlugin::<ArrayTextureMaterial>::default())
-            .insert_resource(MeshTimer{timer: Timer::from_seconds(0.05, TimerMode::Repeating)})
-            .add_systems((mesher::poll_mesh_queue,mesher::queue_meshing,mesh_lod::queue_meshing_lod).in_set(LevelSystemSet::Main))
+        app.add_plugin(MaterialPlugin::<ArrayTextureMaterial>::default())
+            .insert_resource(MeshTimer {
+                timer: Timer::from_seconds(0.05, TimerMode::Repeating),
+            })
+            .add_systems(
+                (
+                    mesher::poll_mesh_queue,
+                    mesher::queue_meshing,
+                    mesh_lod::queue_meshing_lod,
+                )
+                    .in_set(LevelSystemSet::Main),
+            )
             .add_startup_system(init)
-            .add_system(create_chunk_material)
-            ;
+            .add_system(create_chunk_material);
     }
 }
 
@@ -30,26 +49,186 @@ pub struct ChunkMaterial {
     pub opaque_material: Option<Handle<ArrayTextureMaterial>>,
     pub transparent_material: Option<Handle<ArrayTextureMaterial>>,
     pub layers: u32,
-    pub loaded: bool
+    pub loaded: bool,
 }
 
-#[derive(AsBindGroup, Debug, Clone, TypeUuid)]
+/// The GPU representation of the uniform data of a [`StandardMaterial`].
+#[derive(Clone, Default, ShaderType)]
+pub struct ArrayTextureMaterialUniform {
+    /// Doubles as diffuse albedo for non-metallic, specular for metallic and a mix for everything
+    /// in between.
+    pub base_color: Vec4,
+    // Use a color for user friendliness even though we technically don't use the alpha channel
+    // Might be used in the future for exposure correction in HDR
+    pub emissive: Vec4,
+    /// Linear perceptual roughness, clamped to [0.089, 1.0] in the shader
+    /// Defaults to minimum of 0.089
+    pub roughness: f32,
+    /// From [0.0, 1.0], dielectric to pure metallic
+    pub metallic: f32,
+    /// Specular intensity for non-metals on a linear scale of [0.0, 1.0]
+    /// defaults to 0.5 which is mapped to 4% reflectance in the shader
+    pub reflectance: f32,
+    /// The [`StandardMaterialFlags`] accessible in the `wgsl` shader.
+    pub flags: u32,
+    /// When the alpha mode mask flag is set, any base color alpha above this cutoff means fully opaque,
+    /// and any below means fully transparent.
+    pub alpha_cutoff: f32,
+}
+
+#[derive(Clone, PartialEq, Eq, Hash)]
+pub struct ArrayTextureMaterialKey {
+    normal_map: bool,
+    cull_mode: Option<Face>,
+    depth_bias: i32,
+}
+
+#[derive(AsBindGroup, Reflect, FromReflect, Debug, Clone, TypeUuid)]
 //https://www.uuidtools.com/generate/v4
-#[uuid="c275fe2c-7500-46b2-a43d-e3ec8a76f4e4"]
-//TODO: look at standard material source code to get pbr working\
-//for layer
-//https://github.com/bevyengine/bevy/blob/main/examples/shader/custom_vertex_attribute.rs
+#[uuid = "c275fe2c-7500-46b2-a43d-e3ec8a76f4e4"]
+//taken from bevy's StandardMaterial (https://github.com/bevyengine/bevy/blob/a1494e53dfa90e56bd14452b1efb930bd3e79821/crates/bevy_pbr/src/pbr_material.rs#L23)
+#[bind_group_data(ArrayTextureMaterialKey)]
+#[uniform(0, ArrayTextureMaterialUniform)]
 pub struct ArrayTextureMaterial {
-    #[texture(0, dimension = "2d_array")]
-    #[sampler(1)]
-    pub array_texture: Handle<Image>,
+    pub base_color: Color,
+
+    //changed to dimension="2d_array"
+    #[texture(1, dimension = "2d_array")]
+    #[sampler(2)]
+    pub base_color_texture: Option<Handle<Image>>,
+
+    pub emissive: Color,
+    // pub test: StandardMaterial,
+
+    #[texture(3)]
+    #[sampler(4)]
+    pub emissive_texture: Option<Handle<Image>>,
+
+    pub perceptual_roughness: f32,
+
+    pub metallic: f32,
+
+    #[texture(5)]
+    #[sampler(6)]
+    pub metallic_roughness_texture: Option<Handle<Image>>,
+
+    #[doc(alias = "specular_intensity")]
+    pub reflectance: f32,
+
+    #[texture(9)]
+    #[sampler(10)]
+    pub normal_map_texture: Option<Handle<Image>>,
+
+    pub flip_normal_map_y: bool,
+
+    #[texture(7)]
+    #[sampler(8)]
+    pub occlusion_texture: Option<Handle<Image>>,
+    pub double_sided: bool,
+
+    #[reflect(ignore)]
+    pub cull_mode: Option<Face>,
+    pub unlit: bool,
+
+    pub fog_enabled: bool,
+
     pub alpha_mode: AlphaMode,
-    
+
+    pub depth_bias: f32,
+
+    #[texture(11)]
+    #[sampler(12)]
+    pub depth_map: Option<Handle<Image>>,
+    //removed by me
+    // pub parallax_depth_scale: f32,
+
+    // pub parallax_mapping_method: ParallaxMappingMethod,
+
+    // pub max_parallax_layer_count: f32,
+}
+
+impl From<&ArrayTextureMaterial> for ArrayTextureMaterialKey {
+    fn from(material: &ArrayTextureMaterial) -> Self {
+        ArrayTextureMaterialKey {
+            normal_map: material.normal_map_texture.is_some(),
+            cull_mode: material.cull_mode,
+            depth_bias: material.depth_bias as i32,
+        }
+    }
+}
+
+impl AsBindGroupShaderType<ArrayTextureMaterialUniform> for ArrayTextureMaterial {
+    fn as_bind_group_shader_type(&self, images: &RenderAssets<Image>) -> ArrayTextureMaterialUniform {
+        let mut flags = StandardMaterialFlags::NONE;
+        if self.base_color_texture.is_some() {
+            flags |= StandardMaterialFlags::BASE_COLOR_TEXTURE;
+        }
+        if self.emissive_texture.is_some() {
+            flags |= StandardMaterialFlags::EMISSIVE_TEXTURE;
+        }
+        if self.metallic_roughness_texture.is_some() {
+            flags |= StandardMaterialFlags::METALLIC_ROUGHNESS_TEXTURE;
+        }
+        if self.occlusion_texture.is_some() {
+            flags |= StandardMaterialFlags::OCCLUSION_TEXTURE;
+        }
+        if self.double_sided {
+            flags |= StandardMaterialFlags::DOUBLE_SIDED;
+        }
+        if self.unlit {
+            flags |= StandardMaterialFlags::UNLIT;
+        }
+        if self.fog_enabled {
+            flags |= StandardMaterialFlags::FOG_ENABLED;
+        }
+        let has_normal_map = self.normal_map_texture.is_some();
+        if has_normal_map {
+            if let Some(texture) = images.get(self.normal_map_texture.as_ref().unwrap()) {
+                match texture.texture_format {
+                    // All 2-component unorm formats
+                    TextureFormat::Rg8Unorm
+                    | TextureFormat::Rg16Unorm
+                    | TextureFormat::Bc5RgUnorm
+                    | TextureFormat::EacRg11Unorm => {
+                        flags |= StandardMaterialFlags::TWO_COMPONENT_NORMAL_MAP;
+                    }
+                    _ => {}
+                }
+            }
+            if self.flip_normal_map_y {
+                flags |= StandardMaterialFlags::FLIP_NORMAL_MAP_Y;
+            }
+        }
+        // NOTE: 0.5 is from the glTF default - do we want this?
+        let mut alpha_cutoff = 0.5;
+        match self.alpha_mode {
+            AlphaMode::Opaque => flags |= StandardMaterialFlags::ALPHA_MODE_OPAQUE,
+            AlphaMode::Mask(c) => {
+                alpha_cutoff = c;
+                flags |= StandardMaterialFlags::ALPHA_MODE_MASK;
+            }
+            AlphaMode::Blend => flags |= StandardMaterialFlags::ALPHA_MODE_BLEND,
+            AlphaMode::Premultiplied => flags |= StandardMaterialFlags::ALPHA_MODE_PREMULTIPLIED,
+            AlphaMode::Add => flags |= StandardMaterialFlags::ALPHA_MODE_ADD,
+            AlphaMode::Multiply => flags |= StandardMaterialFlags::ALPHA_MODE_MULTIPLY,
+        };
+
+        ArrayTextureMaterialUniform {
+            base_color: self.base_color.as_linear_rgba_f32().into(),
+            emissive: self.emissive.as_linear_rgba_f32().into(),
+            roughness: self.perceptual_roughness,
+            metallic: self.metallic,
+            reflectance: self.reflectance,
+            flags: flags.bits(),
+            alpha_cutoff,
+        }
+    }
 }
 
 //random high id to not conflict
 //would make more sense to be u32, but the texture sampler in the shader doesn't like u32 for some reason
-const ATTRIBUTE_ARRAYTEXTURE_LAYER: MeshVertexAttribute = MeshVertexAttribute::new("TexLayer", 970540917, VertexFormat::Sint32);
+const ATTRIBUTE_ARRAYTEXTURE_LAYER: MeshVertexAttribute =
+    MeshVertexAttribute::new("TexLayer", 970540917, VertexFormat::Sint32);
 
 impl Material for ArrayTextureMaterial {
     fn fragment_shader() -> ShaderRef {
@@ -60,6 +239,9 @@ impl Material for ArrayTextureMaterial {
     }
     fn alpha_mode(&self) -> AlphaMode {
         self.alpha_mode
+    }
+    fn prepass_fragment_shader() -> ShaderRef {
+        PBR_PREPASS_SHADER_HANDLE.typed().into()
     }
     fn specialize(
         _pipeline: &MaterialPipeline<Self>,
@@ -84,44 +266,86 @@ impl Material for ArrayTextureMaterial {
     }
 }
 
-fn init(
-    mut commands: Commands,
-    assets: Res<AssetServer>
-) {
+impl Default for ArrayTextureMaterial {
+    fn default() -> Self {
+        //taken from bevy's StandardMaterial
+        ArrayTextureMaterial {
+            // White because it gets multiplied with texture values if someone uses
+            // a texture.
+            base_color: Color::rgb(1.0, 1.0, 1.0),
+            base_color_texture: None,
+            emissive: Color::BLACK,
+            emissive_texture: None,
+            // Matches Blender's default roughness.
+            perceptual_roughness: 0.5,
+            // Metallic should generally be set to 0.0 or 1.0.
+            metallic: 0.0,
+            metallic_roughness_texture: None,
+            // Minimum real-world reflectance is 2%, most materials between 2-5%
+            // Expressed in a linear scale and equivalent to 4% reflectance see
+            // <https://google.github.io/filament/Material%20Properties.pdf>
+            reflectance: 0.02,
+            occlusion_texture: None,
+            normal_map_texture: None,
+            flip_normal_map_y: false,
+            double_sided: false,
+            cull_mode: Some(Face::Back),
+            unlit: false,
+            fog_enabled: true,
+            alpha_mode: AlphaMode::Opaque,
+            depth_bias: 0.0,
+            depth_map: None,
+            // parallax_depth_scale: 0.1,
+            // max_parallax_layer_count: 16.0,
+            // parallax_mapping_method: ParallaxMappingMethod::Occlusion,
+        }
+    }
+}
+
+fn init(mut commands: Commands, assets: Res<AssetServer>) {
     commands.insert_resource(ChunkMaterial {
         tex_handle: assets.load("textures/tileset.png"),
         opaque_material: None,
         transparent_material: None,
         layers: 0,
-        loaded: false
+        loaded: false,
     });
 }
 
-fn create_chunk_material (
+fn create_chunk_material(
     assets: Res<AssetServer>,
     mut chunk_material: ResMut<ChunkMaterial>,
     mut materials: ResMut<Assets<ArrayTextureMaterial>>,
     mut images: ResMut<Assets<Image>>,
 ) {
-    if matches!(chunk_material.opaque_material, Some(_)) || assets.get_load_state(chunk_material.tex_handle.clone()) != LoadState::Loaded
+    if matches!(chunk_material.opaque_material, Some(_))
+        || assets.get_load_state(chunk_material.tex_handle.clone()) != LoadState::Loaded
     {
         return;
     }
     let image = images.get_mut(&chunk_material.tex_handle).unwrap();
-    //set filtering for clean pixel art
-    image.sampler_descriptor = ImageSampler::nearest();
+    //set filtering for clean pixel art, repeat textures for greedy meshing
+    image.sampler_descriptor =
+        ImageSampler::Descriptor(bevy::render::render_resource::SamplerDescriptor {
+            address_mode_u: AddressMode::Repeat,
+            address_mode_v: AddressMode::Repeat,
+            address_mode_w: AddressMode::Repeat,
+            ..ImageSampler::nearest_descriptor()
+        });
 
     // Create a new array texture asset from the loaded texture.
     let array_layers = 4;
     image.reinterpret_stacked_2d_as_array(array_layers);
     chunk_material.opaque_material = Some(materials.add(ArrayTextureMaterial {
-        array_texture: chunk_material.tex_handle.clone(),
-        alpha_mode: AlphaMode::Opaque
+        base_color_texture: Some(chunk_material.tex_handle.clone()),
+        alpha_mode: AlphaMode::Opaque,
+        ..default()
     }));
     chunk_material.transparent_material = Some(materials.add(ArrayTextureMaterial {
-        array_texture: chunk_material.tex_handle.clone(),
-        alpha_mode: AlphaMode::Blend
-    }));  
+        base_color_texture: Some(chunk_material.tex_handle.clone()),
+        alpha_mode: AlphaMode::Blend,
+        ..default()
+    }));
     chunk_material.layers = array_layers;
     chunk_material.loaded = true;
     info!("Loaded chunk material");
