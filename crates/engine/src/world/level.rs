7a83eb4eb1eef9e1004f76609faaf0d924054246
Jim <jim3moore@protonmail.com>
2024-11-10 22:59:19 -0500

    improve level spawn point algorithm, fixing bug where you sometimes spawn in the ground

--- crates/engine/src/world/level.rs
+++ crates/engine/src/world/level.rs
@@ -740,25 +740,50 @@ impl LevelData {
     }
 
     pub fn get_spawn_point(&self) -> Vec3 {
-        let mut calculated_spawn_point = self.spawn_point;
-        const MAX_CHECK_RANGE: i32 = 1000;
-        for _ in 0..MAX_CHECK_RANGE {
-            match self.get_block(calculated_spawn_point.into()) {
-                Some(BlockType::Empty) => {
-                    if let Some(BlockType::Empty) = self.get_block(
-                        BlockCoord::from(calculated_spawn_point) + BlockCoord::new(0, -1, 0),
-                    ) {
-                        break;
-                    }
-                }
-                Some(_) => {
-                    calculated_spawn_point.y += 1.0;
-                }
-                None => {
-                    break;
-                } //into unloaded chunks
+        let mut calculated_spawn_point: IVec3 = self.spawn_point.as_ivec3();
+        //checks for a 3x3x3 area of air above a 3x1x3 volume that contains at least one non-air block
+        // we will check `CHECK_UP_RANGE` spawns iterating in the +Y direction, then return to where we started and `CHECK_DOWN_RANGE` spawns in the -Y direction
+        // this repeats from each end `MAX_CHECKS` times.
+        //the idea is to prefer spawning above ground, but prefer spawning in the shallow cave than some place way up in the sky.
+        const MIN_SPAWN_VOLUME: IVec3 = IVec3::splat(3);
+        const MAX_CHECKS: i32 = 10;
+        const CHECK_UP_RANGE: i32 = 100;
+        const CHECK_DOWN_RANGE: i32 = 50;
+        for dy in (0..MAX_CHECKS)
+            .map(|i| (0..i * CHECK_UP_RANGE).chain((-i * CHECK_DOWN_RANGE..0).rev()))
+            .flatten()
+        {
+            calculated_spawn_point.y = dy;
+            let ground_volume = Volume::new_inclusive(
+                calculated_spawn_point + IVec3::NEG_Y,
+                calculated_spawn_point + IVec3::new(MIN_SPAWN_VOLUME.x, -1, MIN_SPAWN_VOLUME.z),
+            );
+            let found_ground = ground_volume
+                .iter()
+                .any(|coord| matches!(self.get_block(coord.into()), Some(BlockType::Filled(_))));
+            if !found_ground {
+                info!("didn't find ground at {:?}", calculated_spawn_point);
+                continue;
             }
+            let air_volume = Volume::new_inclusive(
+                calculated_spawn_point + IVec3::Y,
+                calculated_spawn_point + MIN_SPAWN_VOLUME,
+            );
+            let enough_air = air_volume
+                .iter()
+                .all(|coord| matches!(self.get_block(coord.into()), Some(BlockType::Empty) | None));
+            if !enough_air {
+                info!("not enough air at {:?}", calculated_spawn_point);
+                continue;
+            }
+            info!("found spawn at {:?}", calculated_spawn_point);
+            break;
         }
-        calculated_spawn_point
+        let spawn_point = calculated_spawn_point.as_vec3() + MIN_SPAWN_VOLUME.as_vec3() / 2.;
+        info!("spawn point is {:?}", spawn_point);
+        spawn_point
+    }
+    pub fn get_initial_spawn_point(&self) -> Vec3 {
+        self.spawn_point
     }
 }
